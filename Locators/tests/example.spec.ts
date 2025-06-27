e4e38d74a0f6a472957d886db92e24291d6152c4
Daniel Castañeda <deca2402@gmail.com>
2025-06-26 22:40:47 -0500

    Ejercicio de practica

--- Locators/tests/example.spec.ts
+++ Locators/tests/example.spec.ts
@@ -1,18 +1,26 @@
 import { test, expect } from '@playwright/test';
+import { get } from 'http';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('Select Role', async ({ page }) => {
+  await page.goto('https://testertestarudo.com/sandbox-para-pruebas-automatizadas/'); 
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await page.selectOption('#developerRoles', 'backend');
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('Click on Button', async ({ page }) => {
+  await page.goto('https://testertestarudo.com/sandbox-para-pruebas-automatizadas/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.click('text="Click para alerta"');
+  await expect(page.getByText('¡Has presionado el botón de alerta!')).toBeVisible();
+});
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('Complete Form', async ({ page }) => {
+  await page.goto('https://testertestarudo.com/sandbox-para-pruebas-automatizadas/');
+
+  await page.getByLabel("Nombre:").fill('Daniel Castaneda');
+  await page.getByLabel("Email:").fill('test@email.com');
+  await page.fill('input[type="range"]', '25');
+  await page.click('text="Registrarse"');
+
+  await expect(page.getByText('Form submitted!')).toBeVisible();
 });
